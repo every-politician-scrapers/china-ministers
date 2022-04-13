e526caf336de1f1f96a1013a9cc1e28fd7d39db3
Tony Bowden <tony@tmtm.com>
2022-04-13 09:13:27 +0300

    compare enwiki Governors of Fujian

---
+++ sources/governor-fujian/scraper.rb
@@ -0,0 +1,46 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderNonTable < OfficeholderListBase::OfficeholderBase
+  def empty?
+    false
+  end
+
+  def combo_date?
+    true
+  end
+
+  def raw_combo_date
+    raise 'need to define a raw_combo_date'
+  end
+
+  def name_node
+    raise 'need to define a name_node'
+  end
+end
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def holder_entries
+    noko.xpath("//h3[.//span[contains(.,'Governors')]][last()]//following::ol[1]//li[a]")
+  end
+
+  class Officeholder < OfficeholderNonTable
+    def name_node
+      noko.css('a')
+    end
+
+    def raw_combo_date
+      noko.text.split(':').last.gsub(/\(.*\)/, '').tidy
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
